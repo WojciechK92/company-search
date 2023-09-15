d31d75f1725aca33890e5c7d1c6a964fe0582bed
“wojtek19920” <wojtek19920@poczta.onet.pl>
2023-09-15 18:21:08 +0200

    Integrated registration card to backend. After registration user is saved to localStorage and reducer

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,12 +2,19 @@ import { useContext } from 'react';
 import AuthContext from '../context/authContext';
 
 function useAuth() {
-  const setAuth= (newValue) => {
-    newValue ? login() : logout();
+
+  const setAuth= (user) => {
+    if (user) {
+      login(user)
+      window.localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      logout();
+      window.localStorage.setItem('user', null);
+    };
   };
   
-  const { isAuthenticated, login, logout } = useContext(AuthContext);;
-  const auth = isAuthenticated;   
+  const { user, login, logout } = useContext(AuthContext);;
+  const auth = user;   
 
   return [auth, setAuth];
 };
